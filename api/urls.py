6ee9b9656c88779e30fd9dfe89eab2e7325ac9a6
salman <salmankhan.cw@gmail.com>
2026-01-02 19:26:29 +0500

    urls done

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from .views import DocumentViewSet
 
 router = DefaultRouter()
-# router.register('users', UserViewSet)
+router.register('document', DocumentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
