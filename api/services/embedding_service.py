5024c15cbc141133d5358b01c8bcee4f5916ece3
salman <salmankhan.cw@gmail.com>
2026-01-24 18:37:36 +0500

    generate embessings batch

--- api/services/embedding_service.py
+++ api/services/embedding_service.py
@@ -29,4 +29,62 @@ class EmbeddingService:
         Generate embeddings for single text 
         Args: 
             text : text to embed
+        Return:
+            list : embedding vectors
         """
+        model = EmbeddingService.get_model()
+        
+        # Generate embeddings
+        embeddings = model.encode(text, convert_to_numpy=True)
+
+        # Convert to json list and return
+        return embeddings.tolist()
+    
+
+    @staticmethod
+    def generate_embeddings_batch(texts):
+        """
+        Generate embeddings of multiple text at once
+        Args:
+            text : list of text
+        Return:
+            list : list of embedding vectors
+        """
+
+        model = EmbeddingService.get_model()
+        
+        # Generate embeddings of multiple text at once
+        embeddings = model.encode(texts, convert_to_numpy=True, show_progress_bar=True)
+
+        return [emb.tolist() for emb in embeddings]
+    
+
+    @staticmethod
+    def embed_document_chunks(document):
+        """
+        Generate embeddings of all chunks of document
+        Args:
+            document : Document instance
+        Return:
+            int : Number of chunk embended        
+        """
+        # Get all chunks without embeddings
+        chunks = document.chunks.all()
+
+        if not chunks.exists():
+            raise ValueError("No chunk found for this document")
+        
+        # Find exact chunk from the document
+        text = [chunk.content for chunk in chunks]
+
+        # Generate embeddings in batch 
+        print(f"Generating embeddings for {len(text)} chunks ... ")
+        embeddings = EmbeddingService.generate_embeddings_batch(texts)
+
+        # Update chunks with embeddings
+        for chunk, embedding in zip(chunks, embeddings):
+            chunk.embedding = embedding
+            chunk.save()
+        
+        print(f"Successfully embedded {len(chunks)} chunks!")
+        return len(chunks)
